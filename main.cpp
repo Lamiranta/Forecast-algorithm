c25c1c9019e25b56db18fa90e638f01788b16e2f
Lamiranta <63505465+Lamiranta@users.noreply.github.com>
2020-05-24 23:52:37 +0300

    #codeforfun

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <random>
+using namespace std;
+
+/// With this you can play and see different results
+const double dev = 0.7;
+const double a = 0.8;
+
+/// Main variables
+int number_of_data;
+map <int, int> hist;
+double meanError = 0.0;
+double memory[10] = {0}, mean = 0, data, forecast, error = 0;
+
+/// Data initialization
+double input()
+{
+    random_device rd;
+    normal_distribution <double> dist(mean, dev);
+    return floor(100.0 * dist(rd)) / 100.0;
+}
+
+/// Logistic function of weighted sum
+double sigmoid()
+{
+    double sum = 0;
+
+    for (unsigned i = 0; i < 10; ++i)
+        sum += memory[i] * pow(a, i);
+    sum = data / (1 + exp(-sum)) - error;
+
+    return floor(100.0 * sum) / 100.0;
+}
+
+void stage_result()
+{
+    cout << "Forecast: " << forecast << endl;
+    cout << "Actual data: " << data << endl;
+    cout << "Algorithm error: " << error << endl;
+    system("pause");
+}
+
+/// Main process of program
+void data_analysis()
+{
+    /// Memory and mean updating
+    mean = data;
+    for (unsigned i = 9; i > 0; --i)
+        memory[i] = memory[i - 1];
+    memory[0] = data;
+
+    /// Making forecast
+    forecast = sigmoid();
+
+    /// Taking another data
+    data = input();
+
+    /// Checking for error forecast
+    error = forecast - data;
+    ++hist[int(floor(20 * error))];
+    meanError += error;
+
+    /// Some info
+    // stage_result();
+    // system("cls");
+}
+
+int main()
+{
+    cout << setprecision(2) << fixed;
+    cout << "Enter number of data: " << endl;
+    cin >> number_of_data;
+    system("cls");
+
+    /// Data analysis seed (no actual data)
+    data = input();
+
+    /// Actual data analysis
+    for (unsigned i = 0; i < number_of_data; ++i)
+        data_analysis();
+    meanError /= number_of_data;
+
+    /// Data analysis log
+    double bond = 0.0;
+    cout << "Algorithm log: " << endl;
+    cout << "Mean error: " << meanError << endl;
+    for (auto i : hist)
+    {
+        cout << bond << " - " << bond + 0.05 << ": "; bond += 0.05;
+        cout << string(i.second * 100 / number_of_data, '*') << endl;
+    }
+    system("pause");
+
+    return 0;
+}
